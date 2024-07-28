08207cdb43347b6813a12919c5194ce84dfbb787
memo <memo33@users.noreply.github.com>
2024-07-28 09:28:04 +0200

    use BAT4Max sun angle instead of in-game sun

--- source/Sun.py
+++ source/Sun.py
@@ -1,34 +1,39 @@
 import bpy
-from math import radians
+from math import radians, atan2
+from mathutils import Vector
 from .Config import SUN_NAME
 from .Enums import Rotation
 from .Utils import b4b_collection
 
-sun_loc = (0, 0, 1000)  # sun position doesn't matter, just put it somewhere up high and out of the way
-s_x = radians(180)
-s_y = radians(135.0)
-s_z = radians(22.5)  # default to south
+
+_sun_loc = (0, 0, 1000)  # sun position doesn't matter, just put it somewhere up high and out of the way
+
+
+def _bat4max_sun_orientation():
+    # note that sun angle of BATs differs from in-game sun
+    x, y, z = -474, -352, 575  # South sun location in BAT4Max
+    return 0, atan2(Vector([x, y]).length, z), atan2(y, x)  # 0°, 45.757°, 36.598°-180°
 
 
 class Sun:
     @staticmethod
     def get_sun_rotation(rotation):
+        s_x, s_y, s_z = _bat4max_sun_orientation()
         if rotation == Rotation.SOUTH:
-            sr = [s_x, s_y, s_z]
+            return s_x, s_y, s_z
         if rotation == Rotation.EAST:
-            sr = [s_x, s_y, s_z + radians(90)]
+            return s_x, s_y, s_z + radians(90)
         if rotation == Rotation.NORTH:
-            sr = [s_x, s_y, s_z + radians(180)]
+            return s_x, s_y, s_z + radians(180)
         if rotation == Rotation.WEST:
-            sr = [s_x, s_y, s_z + radians(270)]
-        return sr
+            return s_x, s_y, s_z + radians(270)
 
     @staticmethod
     def set_sun(rotation):
         sun = bpy.data.lights.new(SUN_NAME, "SUN")  # name, type
         sun_ob = bpy.data.objects.new(SUN_NAME, sun)
         sun_ob.rotation_mode = "XYZ"
-        sun_ob.location = sun_loc
+        sun_ob.location = _sun_loc
         sun_ob.rotation_euler = rotation
         b4b_collection().objects.link(sun_ob)
         bpy.context.view_layer.update()
